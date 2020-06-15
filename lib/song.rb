2eb5397636c87b056798d9ffd697732ee8e70350
Angela Stephens <angelastephens317@gmail.com>
2020-06-15 23:52:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 class Song
   attr_accessor :name, :artist, :genre 
-  @@count=[]
+  @@count=0
+  @@genres=[]
+  @@artists=[]
   
  def initialize(name,artist,genre)
    @name=name
    @artist=artist
    @genre=genre
    @@count += 1
-   @@artists = []
-   @@genre = []
  end 
 
     def artists=(artists)
