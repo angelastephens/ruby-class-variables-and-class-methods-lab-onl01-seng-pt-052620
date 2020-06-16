5d5ef68d0b2560425ca379dca84686f6f5a1aec8
Angela Stephens <angelastephens317@gmail.com>
2020-06-16 01:56:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,14 +26,16 @@ class Song
   end
 
   def self.genre_count
-    @genre_hash = {}
-    @@genres.each do |genre|
-    if @genre_hash[genre] += 1
-    else
-      @genre_hash[genre] = 1
-  end
+   @genre_hash = {}
+   @@genres.each do |genre|
+     if  @genre_hash[genre]
+       @genre_hash[genre] += 1
+     else
+       @genre_hash[genre] = 1
+     end
+   end
+   @genre_hash
 end
-@genre_hash
 end
 end
 
